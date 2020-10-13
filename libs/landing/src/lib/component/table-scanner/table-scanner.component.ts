9f03c12391bed3f0384ebc5c8d7fcdad6127812b
Johannes Normann Jensen <johannes@gmail.com>
2020-10-13 21:36:02 +0200

    Add table-scanner

---
+++ libs/landing/src/lib/component/table-scanner/table-scanner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fuss-table-scanner',
+  templateUrl: './table-scanner.component.html',
+  styleUrls: ['./table-scanner.component.scss']
+})
+export class TableScannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
