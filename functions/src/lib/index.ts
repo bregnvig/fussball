e230c24c6af690f8aa5e2d75171e335139c181fd
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-14 20:32:46 +0200

    Removed the services

--- functions/src/lib/index.ts
+++ functions/src/lib/index.ts
@@ -1,7 +1,5 @@
 export * from './collection-names';
 export * from './firestore-utils';
-export * from './mail.service';
-export * from './message.service';
 export * from './model';
 export * from './timestamp.converter';
 
