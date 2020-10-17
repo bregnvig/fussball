89f98e60f8da636a111929b9650367af04bad848
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-17 13:15:38 +0200

    Convert timestamps

--- libs/api/src/lib/tables/service/tables.service.ts
+++ libs/api/src/lib/tables/service/tables.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Game, PlayerPosition, Position, Table, TABLES_COLLECTION } from '@fussball/data';
+import { firestoreUtils, Game, PlayerPosition, Position, Table, TABLES_COLLECTION } from '@fussball/data';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { PlayerFacade } from '../../player/+state';
 
 @Injectable({
@@ -10,13 +10,13 @@ import { PlayerFacade } from '../../player/+state';
 })
 export class TablesService {
 
-  tables$: Observable<Table[]> = this.afs.collection<Table>(TABLES_COLLECTION).valueChanges();
+  tables$: Observable<Table[]> = this.afs.collection<Table>(TABLES_COLLECTION).valueChanges().pipe(map(firestoreUtils.convertTimestamps));
 
   constructor(private afs: AngularFirestore, private playerFacade: PlayerFacade) {
   }
 
   table(tableId: string): Observable<Table> {
-    return this.afs.doc<Table>(`${TABLES_COLLECTION}/${tableId}`).valueChanges();
+    return this.afs.doc<Table>(`${TABLES_COLLECTION}/${tableId}`).valueChanges().pipe(map(firestoreUtils.convertTimestamps));
   }
 
   joinGame(tableId: string, position: Position): Observable<any> {
