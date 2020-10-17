467790ac8b171fe1d0028414df8eda4db22d47ba
Johannes Normann Jensen <johannes@gmail.com>
2020-10-17 11:37:09 +0200

    Add join & create func

--- functions/src/app/table.callable.ts
+++ functions/src/app/table.callable.ts
@@ -2,7 +2,7 @@ import { firestore } from 'firebase-admin';
 import { https, region } from 'firebase-functions';
 import { tableURL } from '../lib';
 import { getUid } from '../lib/functions-utils';
-import { isPosition, Position, Table } from '../lib/model';
+import { GameState, isPosition, Position, Table } from '../lib/model';
 
 interface TableRequestData {
     action: 'join';
@@ -22,6 +22,34 @@ function validateData(data: TableRequestData): void {
     }
 }
 
+const joinGame = (table: Table, data: TableRequestData, uid: string): Table => {
+    const game = table.game;
+    const isFullGame = game.latestPosition && Object.keys(game.latestPosition).every(position => !!game.latestPosition[position as Position]);
+    if (isFullGame) {
+        throw new https.HttpsError('failed-precondition', 'Game is full');
+    }
+
+    const isPositionTaken = !!game.latestPosition[data.position];
+    if (isPositionTaken) {
+        throw new https.HttpsError('failed-precondition', `Position '${data.position}' is already taken`);
+    }
+
+    return { ...table, game: { ...game, latestPosition: { ...game.latestPosition, [data.position]: uid } } };
+};
+
+const defaultNumberOfMatches = 3;
+const createGame = (table: Table, data: TableRequestData, uid: string): Table => {
+    return {
+        ...table,
+        game: {
+            latestPosition: { [data.position]: uid },
+            matches: [],
+            numberOfMatches: defaultNumberOfMatches,
+            state: 'preparing',
+        }
+    };
+};
+
 async function joinTable(uid: string, data: TableRequestData): Promise<void> {
     const tableRef = firestore().doc(tableURL(data.tableId));
     return firestore().runTransaction(transaction => {
@@ -29,24 +57,23 @@ async function joinTable(uid: string, data: TableRequestData): Promise<void> {
             const table = tableDoc.data() as Table;
             const game = table.game;
             const state = game.state;
-
-            const gameOngoing = state === 'ongoing';
-            const preparing = state === 'preparing';
-            const full = game.latestPosition && Object.keys(game.latestPosition).every(position => !!game.latestPosition[position as Position]);
-
-            if([gameOngoing, preparing && full].some(x => !!x)) {
-                throw new https.HttpsError('failed-precondition', 'A game is already being played on this table')
-            }
-            if(state === 'preparing' && game.latestPosition[data.position]) {
-                throw new https.HttpsError('failed-precondition', `Position '${data.position}' is already taken`)
+            if (state === 'ongoing') {
+                throw new https.HttpsError('failed-precondition', 'A game is already being played on this table');
             }
 
-        })
-    })
+            if (state === 'preparing') {
+                transaction.set(tableRef, joinGame(table, data, uid));
+            }
+
+            if ((<GameState[]>['completed', 'cancelled']).some(s => state)) {
+                transaction.set(tableRef, createGame(table, data, uid));
+            }
+        });
+    });
 }
 
-export const tableCallable = region('europe-west1').https.onCall( async (data: TableRequestData, context: https.CallableContext) => {
-    
+export const tableCallable = region('europe-west1').https.onCall(async (data: TableRequestData, context: https.CallableContext) => {
+
     const uid = getUid(context);
     validateData(data);
 
