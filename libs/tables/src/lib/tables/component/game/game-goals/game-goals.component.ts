6d359efc052fd2f96500fd75e59b7b2d1b2a90ce
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-17 16:42:51 +0200

    Winning team

--- libs/tables/src/lib/tables/component/game/game-goals/game-goals.component.ts
+++ libs/tables/src/lib/tables/component/game/game-goals/game-goals.component.ts
@@ -14,6 +14,9 @@ export class GameGoalsComponent {
   matches: Match[];
   trackByIndex = (index: number) => index;
 
+  @Input() team1: [string?, string?];
+  @Input() team2: [string?, string?];
+
   @Input() set game(value: Game) {
     this.matches = [...value.matches].reverse();
   }
@@ -26,6 +29,15 @@ export class GameGoalsComponent {
     return !this.isRed(goal);
   }
 
+  winningTeam(match: Match): 'team1' | 'team2' | null {
+    if (match.goals.length >= 8) {
+      const team1GoalFn = (goal: Goal) => this.team1.some(uid => goal.uid === uid);
+      const team1Goals = match.goals.reduce((acc, goal) => acc + (team1GoalFn(goal) ? 1 : 0), 0);
+      const team2Goals = match.goals.length - team1Goals;
+      return team1Goals === 8 ? 'team1' : (team2Goals === 8 ? 'team2' : null);
+    }
+    return null;
+  }
 
 
 }
