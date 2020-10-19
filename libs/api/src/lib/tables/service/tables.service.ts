42b30fafda4a99525d8faad97c638e7f9043a2dc
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-19 20:19:47 +0200

    Move filter to service

--- libs/api/src/lib/tables/service/tables.service.ts
+++ libs/api/src/lib/tables/service/tables.service.ts
@@ -2,21 +2,34 @@ import { Inject, Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { firestoreUtils, JoinTableData, Position, Table, TABLES_COLLECTION } from '@fussball/data';
 import { GoogleFunctions } from '@fussball/firebase';
-import { from, Observable } from 'rxjs';
+import { combineLatest, from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { truthy } from './../../../../../tools/src/lib/rxjs-utils';
+import { PlayerFacade } from './../../player/+state/player.facade';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TablesService {
 
-  tables$: Observable<Table[]> = this.afs.collection<Table>(TABLES_COLLECTION).snapshotChanges().pipe(
+  /**
+   * 
+  */
+  private allTables$: Observable<Table[]> = this.afs.collection<Table>(TABLES_COLLECTION).snapshotChanges().pipe(
     map(snapshot => snapshot.map(s => ({ ...s.payload.doc.data(), id: s.payload.doc.id }))),
     map(firestoreUtils.convertTimestamps)
   );
 
+  tables$: Observable<Table[]> = combineLatest([
+    this.playerFacade.player$.pipe(truthy()),
+    this.allTables$.pipe(truthy())
+  ]).pipe(
+    map(([player, tables]) => tables.filter(t => t.name !== 'Test' || player.roles.includes('admin')))
+  );
+
   constructor(
     private afs: AngularFirestore,
+    private playerFacade: PlayerFacade,
     @Inject(GoogleFunctions) private functions: firebase.functions.Functions) {
   }
 
--- libs/tables/src/lib/tables/tables.component.ts
+++ libs/tables/src/lib/tables/tables.component.ts
@@ -1,28 +1,17 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { PlayerFacade, TablesService } from '@fussball/api';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { TablesService } from '@fussball/api';
 import { Table } from '@fussball/data';
-import { truthy } from '@fussball/tools';
-import { combineLatest, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fussball-tables',
   templateUrl: './tables.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TablesComponent implements OnInit {
+export class TablesComponent {
 
   tables$: Observable<Table[]> = this.service.tables$;
 
-  constructor(private service: TablesService, private playerFacade: PlayerFacade) { }
-
-  ngOnInit(): void {
-    this.tables$ = combineLatest([
-      this.playerFacade.player$.pipe(truthy()),
-      this.service.tables$.pipe(truthy())
-    ]).pipe(
-      map(([player, tables]) => tables.filter(t => t.name !== 'Test' || player.roles.includes('admin')))
-    );
-  }
+  constructor(private service: TablesService,) { }
 
 }
