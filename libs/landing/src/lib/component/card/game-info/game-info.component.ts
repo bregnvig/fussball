260014399c54dc37decf883b60ea9c53aad13c5b
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-19 21:24:07 +0200

    Use onPush

--- libs/landing/src/lib/component/card/game-info/game-info.component.ts
+++ libs/landing/src/lib/component/card/game-info/game-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { TablesService } from '@fussball/api';
 import { Table } from '@fussball/data';
 import { trackByProperty } from '@fussball/tools';
@@ -8,18 +8,19 @@ import { map } from 'rxjs/operators';
 @Component({
   selector: 'fuss-game-info',
   templateUrl: './game-info.component.html',
-  styleUrls: ['./game-info.component.scss']
+  styleUrls: ['./game-info.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GameInfoComponent implements OnInit {
 
   tables$: Observable<Table[]>;
   trackByFn = trackByProperty('name');
-  
+
   constructor(private service: TablesService) { }
 
   ngOnInit(): void {
     this.tables$ = this.service.tables$.pipe(map(tables => tables.filter(t => t.game)));
   }
-  
+
 
 }
