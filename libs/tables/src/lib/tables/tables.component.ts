45278bde44bfeea430d762eadf4ce9509eeaa42e
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-19 20:16:04 +0200

    Remove test whne not admin

--- libs/tables/src/lib/tables/tables.component.ts
+++ libs/tables/src/lib/tables/tables.component.ts
@@ -1,17 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TablesService } from '@fussball/api';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { PlayerFacade, TablesService } from '@fussball/api';
 import { Table } from '@fussball/data';
-import { Observable } from 'rxjs';
+import { truthy } from '@fussball/tools';
+import { combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'fussball-tables',
   templateUrl: './tables.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TablesComponent {
+export class TablesComponent implements OnInit {
 
   tables$: Observable<Table[]> = this.service.tables$;
 
-  constructor(private service: TablesService) { }
+  constructor(private service: TablesService, private playerFacade: PlayerFacade) { }
+
+  ngOnInit(): void {
+    this.tables$ = combineLatest([
+      this.playerFacade.player$.pipe(truthy()),
+      this.service.tables$.pipe(truthy())
+    ]).pipe(
+      map(([player, tables]) => tables.filter(t => t.name !== 'Test' || player.roles.includes('admin')))
+    );
+  }
 
 }
