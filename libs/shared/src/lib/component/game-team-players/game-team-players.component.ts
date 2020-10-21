32562f91532ba2aea626ba39a696ce82fe3ca300
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-21 12:30:03 +0200

    Fix player display

--- libs/shared/src/lib/component/game-team-players/game-team-players.component.ts
+++ libs/shared/src/lib/component/game-team-players/game-team-players.component.ts
@@ -8,8 +8,8 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 })
 export class GameTeamPlayersComponent {
 
-  @Input() defence: string;
-  @Input() offence: string;
+  @Input() top: string;
+  @Input() bottom: string;
   @Input() avatarClass: 'avatar' | 'gameAvatar' = 'gameAvatar';
 
 }
