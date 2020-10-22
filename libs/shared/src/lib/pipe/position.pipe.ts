23f34d4e2d37affea2b6020d784175c0cc4f1cf1
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-22 07:16:15 +0200

    Shorter names

--- libs/shared/src/lib/pipe/position.pipe.ts
+++ libs/shared/src/lib/pipe/position.pipe.ts
@@ -2,10 +2,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Position } from '@fussball/data';
 
 const names = new Map<Position, string>([
-  ['redDefence', 'Rødt forsvar'],
-  ['redOffence', 'Rødt angreb'],
-  ['blueDefence', 'Blåt forsvar'],
-  ['blueOffence', 'Blåt angreb'],
+  ['redDefence', 'Forsvar'],
+  ['redOffence', 'Angreb'],
+  ['blueDefence', 'Forsvar'],
+  ['blueOffence', 'Angreb'],
 ]);
 
 @Pipe({
