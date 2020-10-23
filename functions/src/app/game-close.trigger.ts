c8ffd109c31dc4fefa0e27092c0c150718e03fff
Johannes <johannes@nestech.dk>
2020-10-23 11:11:48 +0200

    Create game trigger

--- functions/src/app/game-close.trigger.ts
+++ functions/src/app/game-close.trigger.ts
@@ -1,34 +1,7 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import { playedURL, tableURL, teamURL } from './../lib/collection-names';
+import { playedURL } from './../lib/collection-names';
 import { Table } from './../lib/model/table.model';
-
-
-const createTeams = async (table: Table): Promise<void> => {
-  const tableRef = admin.firestore().doc(tableURL(table.id));
-  const team1Ref = table.game?.team1?.id ? admin.firestore().doc(teamURL(table.game.team1.id)) : null;
-  const team2Ref = table.game?.team2?.id ? admin.firestore().doc(teamURL(table.game.team2.id)) : null;
-
-  const transactions = [];
-  if (team1Ref) {
-    transactions.push(
-      admin.firestore().runTransaction(transaction => {
-        return transaction.getAll(team1Ref, tableRef).then(([team1Doc, tableDoc]) => {
-          if(!team1Doc.exists) {
-            const team: Team = {...table.game.team1, name: `${table.game.team1.name} & ${}` }
-            transaction.create(team1Ref, team);
-          }
-          return;
-        });
-      })
-    );
-  }
-
-
-
-
-  table.game.team1;
-};
 
 export const gameCloseTrigger = functions.region('europe-west1').firestore.document('tables/{tableId}')
   .onUpdate(async (change, context) => {
@@ -38,9 +11,5 @@ export const gameCloseTrigger = functions.region('europe-west1').firestore.docum
       const db = admin.firestore();
       await db.collection(playedURL(change.after.id)).doc().set(after.game);
     }
-    if (before.game?.state === 'preparing' && after.game?.state === 'ongoing') {
-      await createTeams(after);
-    }
-
     return null;
   });    
