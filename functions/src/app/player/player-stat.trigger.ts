6935e91bde2906be0f10acddaa58ba152993e125
Flemming Bregnvig <flemming@bregnvig.dk>
2020-10-28 16:28:15 +0100

    Create stat object

--- functions/src/app/player/player-stat.trigger.ts
+++ functions/src/app/player/player-stat.trigger.ts
@@ -5,7 +5,12 @@ import { Game, Match, Player, PlayerStat, playerURL, TeamPosition } from '../../
 const numberOfGoals = (match: Match, uid: string): number => match.goals.reduce((acc, goal) => acc + goal.uid === uid && !goal.ownGoal ? 1 : 0, 0);
 const numberOfOwnGoals = (match: Match, uid: string): number => match.goals.reduce((acc, goal) => acc + goal.uid === uid && goal.ownGoal ? 1 : 0, 0);
 const numberOf = (matches: Match[], uid: string, counter: (match: Match, uid: string) => number): number => matches.reduce((acc, match) => acc + counter(match, uid), 0);
-
+const getUpdates = async (db: admin.firestore.Firestore, players: string[], statFn: (player: Player) => PlayerStat): Promise<{ ref: admin.firestore.DocumentReference, stat: PlayerStat; }[]> => {
+  return Promise.all(players.map(async uid => {
+    const snap = await db.doc(playerURL(uid)).get();
+    return { ref: snap.ref, stat: statFn(snap.data() as Player) };
+  }));
+};
 const initalStat = (): PlayerStat => ({ won: 0, lost: 0, goals: 0, ownGoals: 0 });
 
 export const playerStatTrigger = functions.region('europe-west1').firestore.document('tables/{tableId}/played/{playedId}')
@@ -15,9 +20,6 @@ export const playerStatTrigger = functions.region('europe-west1').firestore.docu
 
     const db = admin.firestore();
 
-    const teamRed = [game.latestPosition.redDefence, game.latestPosition.redOffence];
-    const teamBlue = [game.latestPosition.blueDefence, game.latestPosition.blueOffence];
-
     const createStat = (team: TeamPosition) => (player: Player): PlayerStat => {
       const stat: PlayerStat = { ...player.stat || initalStat() };
       stat.goals = numberOf(game.matches, player.uid, numberOfGoals);
@@ -26,17 +28,16 @@ export const playerStatTrigger = functions.region('europe-west1').firestore.docu
       stat.lost += winner !== team ? 1 : 0;
       return stat;
     };
-    return db.runTransaction(async transaction => {
-      const createTeam1Stat = createStat('team1');
-      teamRed.map(async uid => {
-        const snap = await db.doc(playerURL(uid as string)).get();
-        transaction.update(db.doc(playerURL(uid as string)), { stat: createTeam1Stat(snap.data() as Player) });
+
+    const createTeam1Stat = createStat('team1');
+    const team1Updates = await getUpdates(db, game.team1.players, createTeam1Stat);
+    const createTeam2Stat = createStat('team2');
+    const team2Updates = await getUpdates(db, game.team2.players, createTeam2Stat);
+
+    return db.runTransaction(transaction => {
+      [...team1Updates, ...team2Updates].forEach(update => {
+        transaction.update(update.ref, { stat: update.stat });
       });
-      const createTeam2Stat = createStat('team2');
-      teamBlue.map(async uid => {
-        const snap = await db.doc(playerURL(uid as string)).get();
-        transaction.update(db.doc(playerURL(uid as string)), { stat: createTeam2Stat(snap.data() as Player) });
-      });
-      return transaction;
+      return Promise.resolve('Player stat updated');
     });
   });
