b71ba5ef5c9d96d8fc37eeda0ea3b9ca1e800caa
Flemming Bregnvig <flemming@bregnvig.dk>
2020-11-23 17:06:42 +0100

    Relative impure and auto updating

--- libs/shared/src/lib/pipe/relative-to-now.pipe.ts
+++ libs/shared/src/lib/pipe/relative-to-now.pipe.ts
@@ -1,13 +1,25 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { ChangeDetectorRef, OnDestroy, Pipe, PipeTransform } from '@angular/core';
 import { DateTime } from 'luxon';
+import { interval, Subscription } from 'rxjs';
 
 @Pipe({
   name: 'relativeToNow',
+  pure: false
 })
-export class RelativeToNowPipe implements PipeTransform {
+export class RelativeToNowPipe implements PipeTransform, OnDestroy {
 
+
+  #subscription: Subscription;
+
+  constructor(cd: ChangeDetectorRef) {
+    this.#subscription = interval(1000).subscribe(() => cd.markForCheck());
+  }
+
+  ngOnDestroy() {
+    this.#subscription?.unsubscribe();
+  }
   transform(value: DateTime | null | undefined): string | null | undefined {
-    return value instanceof DateTime ? value.toRelative({locale: 'da'}) : value;
+    return value instanceof DateTime ? value.toRelative({ locale: 'da' }) : value;
   }
 }
 
